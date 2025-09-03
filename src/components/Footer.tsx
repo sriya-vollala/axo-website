3627d7fe60ba1e05f97b001d1c1211c403aee056
Darcy Hudson <darcyhudson@ucsb.edu>
2025-09-03 16:39:10 -0700

    added insta link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,28 @@
+import { FaInstagram } from 'react-icons/fa';
+
+// create and
+const InstagramLink = () => {
+    return (
+        <a
+            href="https://www.instagram.com/axoucsb/"
+            // target and rel for security/fun
+            target="_blank"
+            rel="noopener noreferrer"
+            className="instagram-link"
+        >
+            <FaInstagram size={20} />
+        </a>
+    );
+};
+
 function Footer() {
     return (
-        <p className="footer">© 2025 Alpha Chi Omega, Delta Psi Chapter | All Rights Reserved.</p>
+        <footer className="footer">
+            <div><InstagramLink /></div>
+            <p>
+                © 2025 Alpha Chi Omega, Delta Psi Chapter | All Rights Reserved.
+            </p>
+        </footer>
     )
 }
 
