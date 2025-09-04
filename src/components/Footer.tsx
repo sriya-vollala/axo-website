ea96773546746e9bd1beafa149cabdd319bc08f5
Darcy Hudson <darcyhudson@ucsb.edu>
2025-09-03 21:35:55 -0700

    updated insta link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,25 @@
-import { FaInstagram } from 'react-icons/fa';
-
-// create icon variable
-const InstagramLink = () => {
-    return (
-        // <a> makes it a clickable icon
-        <a
-            // website link the icon redirects to
-            href="https://www.instagram.com/axoucsb/"
-            // target and rel for security/fun
-            target="_blank"
-            rel="noopener noreferrer"
-            className="instagram-link"
-        >
-            <FaInstagram size={20} />
-        </a>
-    );
-};
+import React from 'react';
+import instagramLogo from '../assets/instagram.png'; // Correctly import the image
 
 function Footer() {
     return (
         <footer className="footer">
-            <div><InstagramLink /></div>
+            <div>
+                {/* Use the imported image variable as the 'src' */}
+                <a
+                    href="https://www.instagram.com/axoucsb/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="instagram-link"
+                >
+                    <img src={instagramLogo} alt="Instagram Logo" className="instagram-icon" />
+                </a>
+            </div>
             <p>
                 © 2025 Alpha Chi Omega, Delta Psi Chapter | All Rights Reserved.
             </p>
         </footer>
-    )
+    );
 }
 
 export default Footer;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,4 +30,3 @@ function NavBar() {
     
 }
 export default NavBar;
-
